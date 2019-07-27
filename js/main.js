e475522bdc81eaf4731789a320c7d57d8b093b52
Abror Anvarov <46844042+aanvarov@users.noreply.github.com>
2019-07-28 03:16:21 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+$(function() {
+	var $a = $("#tabs li");
+	$a.click(function() {
+		$a.removeClass("active");
+		$(this).addClass("active");
+	});
+});
